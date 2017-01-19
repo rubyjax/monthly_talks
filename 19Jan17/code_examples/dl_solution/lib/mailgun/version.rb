1ab357e548b15d23badfda314e48d1320687dc67
DL Cummings <444c43@gmail.com>
2017-01-19 10:43:44 -0500

    initial commit

---
+++ 19Jan17/code_examples/dl_solution/lib/mailgun/version.rb
@@ -0,0 +1,3 @@
+module Mailgun
+  VERSION = "0.1.0"
+end
---
+++ 19Jan17/code_examples/dl_solution/spec/mailgun_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Mailgun do
+  it 'has a version number' do
+    expect(Mailgun::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
